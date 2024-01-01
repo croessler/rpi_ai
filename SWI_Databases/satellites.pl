3444aab4687c9ffdb7124be1cf16a8810c75373e
Christian Roessler <mail@croessler.com>
2024-01-01 21:06:31 +0100

    Rename (2)

--- SWI_Databases/satellites.pl
+++
@@ -1,9 +0,0 @@
-%% a simple Prolog knowledge base
-%% facts
-orbits(earth, sun).
-orbits(saturn, sun).
-orbits(titan, saturn).
-%% rules
-satellite(X) :- orbits(X, _).
-planet(X) :- orbits(X, sun).
-moon(X) :- orbits(X, Y), planet(Y).
